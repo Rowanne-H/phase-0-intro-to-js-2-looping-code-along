2e5e9e028ab21698806ebd8782a1dbd24e799dc7
YUERONG HUANG <rowanne.h@hotmail.com>
2024-01-13 00:41:15 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(arr, event) {
+    let newArr = []
+    for (let i=0; i<arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr;
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int);
+        int--;
+    }
+}
+countDown(10)
